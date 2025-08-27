df126c2e30eb1227b197b13d6b49487ddbaf5920
João Pedro <joaolopes19032003@gmail.com>
2025-08-27 18:10:46 -0300

    git actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/#',
+    baseUrl: "http://lojaebac.ebaconline.art.br/#",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "mochawesome-report",
+      overwrite: false,
+      reportFilename: "index.html",
+      html: true,
+      json: false,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
