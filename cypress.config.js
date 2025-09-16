b92fe020621fa14a83ad3712fa8d12f94d860ed0
João Pedro <joaolopes19032003@gmail.com>
2025-09-16 17:47:29 -0300

    dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,10 @@
 const { defineConfig } = require("cypress");
-const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
 
 module.exports = defineConfig({
   projectId: "jysd74",
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/#',
     setupNodeEvents(on, config) {
-      return registerReportPortalPlugin(on, config);
       // implement node event listeners here
     },
   },
